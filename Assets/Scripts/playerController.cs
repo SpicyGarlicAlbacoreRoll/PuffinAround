fbcfa3428812180f0911fca6b29cc88fc156de9a
Kim <jdfairbanks@alaska.edu>
2019-10-12 11:25:46 -0800

    moving player controller towards raycast system

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -6,15 +6,49 @@ public class playerController : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody2D playerRB;
+    public LayerMask groundLayer;
+
+    public Vector2 position;
+    public Vector2 velocity;
+    public Vector2 acceleration;
+    public float gravity = 9.8f;
     void Start()
     {
-        playerRB = gameObject.GetComponent<Rigidbody2D>();
+        // playerRB = gameObject.GetComponent<Rigidbody2D>();
+        position = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 xAxisMovement = new Vector2(Input.GetAxisRaw("Horizontal"), 0.0f);
-        playerRB.AddForce(xAxisMovement);
+        // playerRB.AddForce(xAxisMovement);
+
+        if(!IsGrounded()) {
+            Falling();
+        }
+    }
+
+    void Move() {
+
+    }
+
+    bool IsGrounded() {
+        Vector2 direction = Vector2.down;
+        Vector2 position = transform.position;
+
+        float distance = 1.0f;
+
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
+        if(hit.collider != null) {
+            return true;
+        }
+
+        return false;
+    }
+
+    void Falling() {
+        position -= (Vector2.up * gravity * Time.deltaTime);
+        transform.position = position;
     }
 }
