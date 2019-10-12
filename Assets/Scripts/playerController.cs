f63cc04f9510cabdbffca4d54c28b80d4ebd4321
Kim <jdfairbanks@alaska.edu>
2019-10-12 10:51:06 -0800

    added a moving sprite on screen, controllable by a and d keys

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D playerRB;
+    void Start()
+    {
+        playerRB = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 xAxisMovement = new Vector2(Input.GetAxisRaw("Horizontal"), 0.0f);
+        playerRB.AddForce(xAxisMovement);
+    }
+}
