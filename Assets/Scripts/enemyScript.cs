972f1fafd8b116ddaa61b6b97193c8c14ea2cda1
Andrew Adler <gleeb101@gmail.com>
2019-10-12 17:04:50 -0800

    Enemies can see the player.

--- Assets/Scripts/enemyScript.cs
+++ Assets/Scripts/enemyScript.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class enemyScript : MonoBehaviour
 {
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("pufffinPlaceholder");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 playerPos = player.transform.position;
+        print("player position: (" + playerPos.x + ", " + playerPos.y + ")");
     }
 
     void OnTriggerEnter2D(Collider2D other) {
